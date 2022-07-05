a56f0dcb7195ed82bdfeb4134c6834d673135f00
Simone-Alghisi <alghisius.simone@gmail.com>
2022-07-05 11:42:14 +0200

    update README, remove useless stuff from flake.nix, add .envrc

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# domain-adaptation-project
-A repository containing the Domain Adaptation notebook for the Deep Learning course of the University of Trento (AA 2021-2022)
+# Unsupervised Domain Adaptation (UDA) Project
+A repository containing the Domain Adaptation notebook for the Deep Learning course of the University of Trento (AA 2021-2022).
+
+## Group Members
+* [Simone Alghisi](https://github.com/Simone-Alghisi/)
+* [Massimo Rizzoli](https://github.com/massimo-rizzoli)
+
+## Proposed techniques
+For this project we've used Discrimination Based Techniques for the most, by relying on the popular MMD loss for understanding the domain alignment. In general, we have implemented
+
+* *Deep Domain Confusion* (between single and multiple layers), i.e. the classic implementation of MMD loss for solving UDA problems;
+* *DSAN* (Deep Subdomain Adaptation Network), i.e. a more recent approach based on subdomain alignment;
+* *DRCN* (Deep Reconstruction Classification Network), i.e. a different approach for UDA based on a multi-task approach using a decoder together with the classifier.
+
+For more in depth details, please refer to the [notebook](./domain-adaptation-project.ipynb).
+
+## Notes
+Please keep in mind that the following project has been developed from Google Colab. To avoid possible incompatibilities issues, please load it and run it from there.
--- flake.nix
+++ flake.nix
@@ -9,46 +9,15 @@
   outputs = {self, nixpkgs, flake-utils, mach-nix }@inp:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      # you can build automatically python pkgs from repo using their requirements
-      # poke-env = mach-nix.lib."${system}".buildPythonPackage {
-      #   pname = "poke_env";
-      #   version = "0.4.21";
-      #   src = pkgs.fetchFromGitHub {
-      #     owner = "Simone-Alghisi";
-      #     repo = "poke-env";
-      #     rev = "612b5862df5a3549e8c93ecee686ba38dcb86f95";
-      #     hash = "sha256-TJ+1e2WLKsQmQUvCPVLPphEtglnQwkbPmLNb5LA9Aro=";
-      #   };
-      # };
       python-build = mach-nix.lib."${system}".mkPython {
-        # you can specify the pyhton version
-        # python = "python39"
-        requirements = builtins.readFile ./requirements.txt;
-        packagesExtra = [
-          # you can build tarball, local paths
-          # ./path/to/package
-          # (builtins.fetchTarball {
-          #   url = "https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.1.0/en_core_web_sm-3.1.0.tar.gz";
-          #   sha256 = "0iqhcmwvl2jna6m2zk6g9j1j7six6wf7kzd2xlxgypn0s8649dvb";
-          # })
-        ];
+        python = "python39";
+        requirements = builtins.readFile ./requirements-dev.txt;
       };
-      # you can override packages
-      #Qt-matplotlib = pkgs.python39Packages.matplotlib.override {
-      #  enableQt = false;
-      #  enableTk = true;
-      #}; 
     in
     {
-      # enter this python environment by executing `nix shell .`
       devShell = pkgs.mkShell {
-        # you can hook some variables for example
-        shellHook = ''
-          export QT_QPA_PLATFORM=wayland-egl
-        '';
         buildInputs = [
           python-build
-          # Additional packages, e.g. pkgs.python39Packages.matplotlib
         ];
       };
     });
